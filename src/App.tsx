e5bab07f77e6e5d1a11fa9675ce37d626ff10746
zntb <zntbdev@proton.me>
2024-02-26 19:33:01 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from './components/ui/button.tsx';
+
+function App() {
+  return (
+    <>
+      <Button>Click me!</Button>
+    </>
+  );
+}
+
+export default App;
