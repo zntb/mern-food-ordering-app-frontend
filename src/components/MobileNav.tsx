48bd0cfac228a5d56b8b4e104ec18908ee1f975f
zntb <zenetibi@yahoo.com>
2024-02-29 08:01:27 +0200

    Add mobile nav

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,31 @@
+import { Menu } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet.tsx';
+import { Separator } from './ui/separator.tsx';
+import { Button } from './ui/button.tsx';
+
+function MobileNav() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          <span> Welcome to MernEats.com!</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default MobileNav;
