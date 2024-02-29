88c3479c18153a6fe808f084278d12829520b767
zntb <zntbdev@proton.me>
2024-02-29 08:47:39 +0200

    Add responsive landing page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,10 +1,18 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/layout.tsx';
+import HomePage from './pages/HomePage.tsx';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout>Home Page</Layout>} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
       <Route path="/user-profile" element={<span>User Profile Page</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,4 +1,6 @@
+import Footer from '@/components/Footer.tsx';
 import Header from '@/components/Header.tsx';
+import Hero from '@/components/Hero.tsx';
 
 type Props = {
   children: React.ReactNode;
@@ -8,8 +10,9 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
+      <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
     </div>
   );
 };
