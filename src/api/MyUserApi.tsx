95c75461d4af20f6e721044e0fc3fca00acc96bd
zntb <zenetibi@yahoo.com>
2024-02-29 14:54:23 +0200

    Create user request

---
+++ src/api/MyUserApi.tsx
@@ -0,0 +1,40 @@
+// import { User } from '@/types';
+// import { useAuth0 } from '@auth0/auth0-react';
+import { useMutation } from 'react-query';
+
+const API_BASE_URL = 'http://localhost:5000';
+
+type CreateUserRequest = {
+  auth0Id: string;
+  email: string;
+};
+
+export const useCreateMyUser = () => {
+  const createMyUserRequest = async (user: CreateUserRequest) => {
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to create user');
+    }
+  };
+
+  const {
+    mutateAsync: createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useMutation(createMyUserRequest);
+
+  return {
+    createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  };
+};
