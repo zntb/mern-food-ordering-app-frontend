e43a6a90eb2e1b189de2ca25c9a45aacea02d9cc
zntb <zenetibi@yahoo.com>
2024-02-29 19:00:05 +0200

    Add User profile form

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/layout.tsx';
 import HomePage from './pages/HomePage.tsx';
 import AuthCallbackPage from './pages/AuthCallbackPage.tsx';
+import UserProfilePage from './pages/UserProfilePage.tsx';
 
 const AppRoutes = () => {
   return (
@@ -9,13 +10,20 @@ const AppRoutes = () => {
       <Route
         path="/"
         element={
-          <Layout>
+          <Layout showHero>
             <HomePage />
           </Layout>
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route path="/user-profile" element={<span>User Profile Page</span>} />
+      <Route
+        path="/user-profile"
+        element={
+          <Layout>
+            <UserProfilePage />
+          </Layout>
+        }
+      />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
