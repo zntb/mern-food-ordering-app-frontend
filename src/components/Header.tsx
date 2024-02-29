431a61623db523f23077190e279a5b9421139036
zntb <zenetibi@yahoo.com>
2024-02-29 08:10:11 +0200

    Add desktop nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import MobileNav from './MobileNav.tsx';
+import MainNav from './MainNav.tsx';
 
 function Header() {
   return (
@@ -13,6 +14,9 @@ function Header() {
         </Link>
         <div className="md:hidden">
           <MobileNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
         </div>
       </div>
     </div>
