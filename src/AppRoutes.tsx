7f13279c785a9a797ad3506cdc869d0a1d1575b5
zntb <zntbdev@proton.me>
2024-03-01 08:42:16 +0200

    Protect user profile router

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Layout from './layouts/layout.tsx';
 import HomePage from './pages/HomePage.tsx';
 import AuthCallbackPage from './pages/AuthCallbackPage.tsx';
 import UserProfilePage from './pages/UserProfilePage.tsx';
+import ProtectedRoute from './auth/ProtectedRoute.tsx';
 
 const AppRoutes = () => {
   return (
@@ -16,14 +17,18 @@ const AppRoutes = () => {
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route
-        path="/user-profile"
-        element={
-          <Layout>
-            <UserProfilePage />
-          </Layout>
-        }
-      />
+
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
+
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
