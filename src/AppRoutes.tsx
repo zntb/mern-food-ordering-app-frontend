192d8b89deed8548dc03f7c67a5c0ca1d5151a46
zntb <zenetibi@yahoo.com>
2024-03-01 18:28:55 +0200

    Create restaurant form

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage.tsx';
 import AuthCallbackPage from './pages/AuthCallbackPage.tsx';
 import UserProfilePage from './pages/UserProfilePage.tsx';
 import ProtectedRoute from './auth/ProtectedRoute.tsx';
+import ManageRestaurantPage from './pages/ManageRestaurantPage.tsx';
 
 const AppRoutes = () => {
   return (
@@ -24,6 +25,14 @@ const AppRoutes = () => {
           element={
             <Layout>
               <UserProfilePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
             </Layout>
           }
         />
---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,7 @@
+import ManageRestaurantForm from '@/forms/manage-restaurant-form/ManageRestaurantForm.tsx';
+
+function ManageRestaurantPage() {
+  return <ManageRestaurantForm />;
+}
+
+export default ManageRestaurantPage;
