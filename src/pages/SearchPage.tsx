e3d53f8fc9d892a7d8a63b90e3652e9ef0beafee
zntb <zenetibi@yahoo.com>
2024-03-02 17:50:37 +0200

    Add search API request

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,9 +1,22 @@
+import { useSearchRestaurants } from '@/api/RestaurantApi.tsx';
 import { useParams } from 'react-router-dom';
 
 function SearchPage() {
   const { city } = useParams();
+  const { results } = useSearchRestaurants(city);
 
-  return <span>User Searched for {city}</span>;
+  return (
+    <span>
+      User Searched for {city}{' '}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
 }
 
 export default SearchPage;
