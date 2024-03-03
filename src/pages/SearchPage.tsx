293fee3819f6aac01fe7fefe4e8dd0c8fe73d94a
zntb <zenetibi@yahoo.com>
2024-03-03 10:07:20 +0200

    Add Restaurant Test Data and Search Filter

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,45 @@
-import { useSearchRestaurants } from '@/api/RestaurantApi.tsx';
-import SearchResultCard from '@/components/SearchResultCard.tsx';
-import SearchResultInfo from '@/components/SearchResultInfo.tsx';
+import { useSearchRestaurants } from '@/api/RestaurantApi';
+
+import SearchBar, { SearchForm } from '@/components/SearchBar';
+import SearchResultCard from '@/components/SearchResultCard';
+import SearchResultInfo from '@/components/SearchResultInfo';
+
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-function SearchPage() {
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+  sortOption: string;
+};
+
+const SearchPage = () => {
   const { city } = useParams();
-  const { results, isLoading } = useSearchRestaurants(city);
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: '',
+    page: 1,
+    selectedCuisines: [],
+    sortOption: 'bestMatch',
+  });
+
+  const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery,
+      page: 1,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: '',
+      page: 1,
+    }));
+  };
 
   if (isLoading) {
     <span>Loading ...</span>;
@@ -17,14 +51,24 @@ function SearchPage() {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-      <div id="cuisines-list">insert cuisines here :)</div>
-      <div id="main-content" className="flex flex-col gap-5"></div>
-      <SearchResultInfo total={results.pagination.total} city={city} />
-      {results.data.map((restaurant) => (
-        <SearchResultCard restaurant={restaurant} />
-      ))}
+      <div id="cuisines-list"></div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder="Search by Cuisine or Restaurant Name"
+          onReset={resetSearch}
+        />
+        <div className="flex justify-between flex-col gap-3 lg:flex-row">
+          <SearchResultInfo total={results.pagination.total} city={city} />
+        </div>
+
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default SearchPage;
