b12c50f8bdb7d9b69c873bf418fb663af706ce5d
zntb <zenetibi@yahoo.com>
2024-03-03 14:57:16 +0200

    Update user profile form

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -18,7 +18,7 @@ import { useEffect } from 'react';
 
 const formSchema = z.object({
   email: z.string().optional(),
-  name: z.string().min(1, 'Name is required'),
+  name: z.string().min(1, 'name is required'),
   addressLine1: z.string().min(1, 'Address Line 1 is required'),
   city: z.string().min(1, 'City is required'),
   country: z.string().min(1, 'Country is required'),
