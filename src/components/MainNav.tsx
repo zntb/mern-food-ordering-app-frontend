93551ac31489157ce56ef20e064d16e4bf22768e
zntb <zenetibi@yahoo.com>
2024-03-04 12:09:08 +0200

    Add Order status config

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,13 +1,19 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from './ui/button.tsx';
 import UsernameMenu from './UsernameMenu.tsx';
+import { Link } from 'react-router-dom';
 
 function MainNav() {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
     <span className="flex space-x-2 items-center">
       {isAuthenticated ? (
-        <UsernameMenu />
+        <>
+          <Link to="/order-status" className="font-bold hover:text-orange-500">
+            Order Status
+          </Link>
+          <UsernameMenu />
+        </>
       ) : (
         <Button
           variant="ghost"
