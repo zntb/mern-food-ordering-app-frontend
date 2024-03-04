dafab86aa5e2486efe5da2aa4ba178fe1527ee49
zntb <zenetibi@yahoo.com>
2024-03-04 12:39:41 +0200

    Add Restaurant Orders page

--- src/components/PaginationSelector.tsx
+++ src/components/PaginationSelector.tsx
@@ -31,8 +31,8 @@ const PaginationSelector = ({ page, pages, onPageChange }: Props) => {
           </PaginationItem>
         )}
 
-        {pageNumbers.map((number) => (
-          <PaginationItem>
+        {pageNumbers.map((number, index) => (
+          <PaginationItem key={index}>
             <PaginationLink
               href="#"
               onClick={() => onPageChange(number)}
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -104,8 +104,8 @@ const SearchPage = () => {
           />
         </div>
 
-        {results.data.map((restaurant) => (
-          <SearchResultCard restaurant={restaurant} />
+        {results.data.map((restaurant, index) => (
+          <SearchResultCard key={index} restaurant={restaurant} />
         ))}
         <PaginationSelector
           page={results.pagination.page}
